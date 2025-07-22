a7f7c6a4e09eaa339e1131c9764d6ea673e3feb2
150610 <henryp4211@masseyhigh.school.nz>
2025-07-22 12:55:39 +1200

    vid 2

---
+++ video 2.py
@@ -0,0 +1,28 @@
+# Code to learn about variables...
+
+#Variables can be a string (note the speech marks)
+greeting="Hello World"
+
+#Variables can be a number
+fav_num=7
+num_string ="5"
+
+print(fav_num * 2)
+print(num_string * 5)
+
+# Constants are uppercase
+PI= 3.14
+
+a_o1_my_num=7
+
+
+
+#Variables can be Boolean (True/False)
+love_programming=False
+
+print(greeting)
+print(fav_num)
+print(love_programming)
+
+#This variable has a problem
+my_name = "name"
